74ca752c0cc5f4c3143a0bedd8835e971de20b3d
Anghelo Alagon <200518@unsaac.edu.pe>
2022-11-25 20:21:22 -0500

    se agrego el navBar

--- src/AppSchedule.jsx
+++ src/AppSchedule.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate} from 'react-router-dom';
+import { NavBar } from './components/NavBar';
 import { TodoProvider } from './context/TodoProvider';
 import { HomePage } from './pages/HomePage';
 import { SchedulePage } from './pages/SchedulePage';
@@ -7,6 +8,8 @@ export const AppSchedule = () => {
 
   return (
     <TodoProvider>
+      <NavBar/>
+      
       <Routes>
         <Route path="/" element={ <HomePage/> }/>
         <Route path="horario" element={ <SchedulePage/> }/>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link, NavLink } from 'react-router-dom'
+
+export const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/" >Horario de Elmo</Link>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className={({ isActive }) => `nav-link ${isActive ? `active` : ''}`} to="/" >Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={({ isActive }) => `nav-link ${isActive ? `active` : ''}`} to="/horario" >Horario</NavLink>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
