0b719133498520a33bd2a02898b2a9dbbacc1f3c
Anghelo Alagon <200518@unsaac.edu.pe>
2022-11-26 15:58:48 -0500

    se arreglo el error de useId

--- src/components/ScheduleTr.jsx
+++ src/components/ScheduleTr.jsx
@@ -2,17 +2,20 @@ import { useId } from "react";
 import { ScheduleTrChild } from "./ScheduleTrChild";
 
 export const ScheduleTr = ({ courses }) => {
+  const idTr = useId();
+
+
   return (
     <>
       {
         courses.map((item) => {
           if (item.length > 1) {
             return (
-              <td key={ useId() } >
+              <td key={ idTr } >
                 <div className="h-100 d-flex flex-column bordeTd">
                   {
                     item.map((course) => (
-                      <ScheduleTrChild key={ useId() } course={ course }/>
+                      <ScheduleTrChild key={ idTr + course.id } course={ course }/>
                     ))
                   }
                 </div>
@@ -20,7 +23,7 @@ export const ScheduleTr = ({ courses }) => {
             )
           }
           return (
-            <td key={ useId() } style={{ backgroundColor: item[0]?.color }} >
+            <td key={ idTr } style={{ backgroundColor: item[0]?.color }} >
               {item[0]?.id}
             </td>
           );
