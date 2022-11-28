7c2bd14fb63bd31f66c94fd61ec99368e07eb89e
Anghelo Alagon <200518@unsaac.edu.pe>
2022-11-27 22:12:24 -0500

    se agrego la funcion descargar

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,10 +1,10 @@
 import { ScheduleBody } from "./ScheduleBody";
 import { ScheduleHead } from "./ScheduleHead"
 
-export const Schedule = () => {  
+export const Schedule = ({ exportRef }) => { 
   
   return (
-    <table className="table h-100 table-bordered">
+    <table className="table h-100 table-bordered" ref={exportRef}>
       <ScheduleHead/>
 
       <ScheduleBody/>
--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -1,13 +1,18 @@
 import { Courses } from "../components/Courses"
 import { Schedule } from "../components/Schedule";
+import exportAsImage from "../help/exportAsImage";
+import { useRef } from "react";
 import './../app.css';
 
-
 export const SchedulePage = () => {
+  const exportRef = useRef();
   return (
     <div className="countainer p-3 vh-100">
-      <div className="h-5">
+      <div className="h-5 position-relative">
         <h1 className="text-center" >Horario</h1>
+        <button className="btn btn-primary position-absolute top-50 end-0 translate-middle-y" onClick={() => exportAsImage(exportRef.current, "test")}>
+          Descargar imagen
+        </button>
         <hr />
       </div >
 
@@ -22,7 +27,7 @@ export const SchedulePage = () => {
 
         {/* schedule */}
         <div className="col-md-9 p-5 align-items-center row">
-          <Schedule />
+          <Schedule exportRef={exportRef} />
         </div>
 
       </section>
