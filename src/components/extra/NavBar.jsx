552cfcaa298c3e0d78a511d88a90cf12756a9466
Anghelo Alagon <200518@unsaac.edu.pe>
2022-11-30 18:35:34 -0500

    se agrego el img y el ico

--- src/components/extra/NavBar.jsx
+++ src/components/extra/NavBar.jsx
@@ -4,14 +4,14 @@ export const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/" ><img src="./../../../img/elmo.png" alt="elmo" height="40" width="33" class="d-inline-block align-text-top"/> ELMOrario</Link>
+        <Link className="navbar-brand" to="/" ><img src="./../../../img/elmo.png" alt="elmo" height="40" width="33" className="d-inline-block align-text-top"/> ELMOrario</Link>
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <NavLink className={({ isActive }) => `nav-link ${isActive ? `active` : ''}`} to="/" >Home</NavLink>
             </li>
-            
+
             <li className="nav-item">
               <NavLink className={({ isActive }) => `nav-link ${isActive ? `active` : ''}`} to="/horario" >Horario</NavLink>
             </li>
